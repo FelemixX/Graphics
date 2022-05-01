3c478918d10a30a30b07b40d96a14d5be2f4ca4a
FelemixX <83292474+FelemixX@users.noreply.github.com>
2022-05-02 02:09:03 +0300

    Add files via upload

---
+++ lab3spline/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace lab3spline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1334, 653);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.onMouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 668);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ lab3spline/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3spline
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        List<Point> points;
+        Pen penMain, penSecondary, penBlue;
+        int listSize = 255;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            points = new List<Point>(listSize);
+            penMain = new Pen(Brushes.Black);
+            penSecondary = new Pen(Brushes.Red);
+            penBlue = new Pen(Brushes.Blue);
+        }
+
+        void spline2()
+        {
+            float oldX, oldY, newX, newY, u;
+            for (int i = 0; i < points.Count - 2; i++)
+            {
+                oldX = (points[i].X + points[i + 1].X) / 2;
+                oldY = (points[i].Y + points[i + 1].Y) / 2;
+                for (u = 0; u <= 1; u += 0.01F)
+                {
+                    newX = 0.5F * (1F - u) * (1F - u) * points[i].X + (0.75F - (u - 0.5F) * (u - 0.5F)) * points[i + 1].X + 0.5F * u * u * points[i + 2].X; //Сплайн второго порядка
+                    newY = 0.5F * (1F - u) * (1F - u) * points[i].Y + (0.75F - (u - 0.5F) * (u - 0.5F)) * points[i + 1].Y + 0.5F * u * u * points[i + 2].Y;
+                    double tmpX = Math.Round(oldX);
+                    double tmpY = Math.Round(oldY);
+                    double tmpXnew = Math.Round(newX);
+                    double tmpYnew = Math.Round(newY);
+                    g.DrawLine(penBlue, new Point((int)tmpX, (int)tmpY), new Point((int)tmpXnew, (int)tmpYnew));
+                    oldX = newX;
+                    oldY = newY;
+                }
+            }
+        }
+
+        private void onMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drawCurvePart(e.X, e.Y, penMain);
+            }
+            else
+            {
+                spline2();
+                g.Dispose();
+                points.Clear();
+                g = pictureBox1.CreateGraphics();
+            }
+        }
+
+
+        private void drawCurvePart(int x, int y, Pen pen)
+        {
+            g.DrawRectangle(pen, x, y, 1, 1);
+            if (points.Count > 0)
+            {
+                g.DrawLine(penMain, points.Last(), new Point(x, y));
+            }
+            points.Add(new Point(x, y));
+        }
+    }
+}
